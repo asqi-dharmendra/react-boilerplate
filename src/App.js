ef6323ddb4591d1f5da2f1b11525521a8d1598a3
asqi-dharmendra <dharmendra@asqi.in>
2024-08-22 11:18:06 +0530

    chore: Add initial project files and configuration

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          I am a boilerplate react app.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
